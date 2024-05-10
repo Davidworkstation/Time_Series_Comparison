e56e7faaea88ab34269ac1fcb0d0e0886852851b
Risaiah <db2019@hawaii.edu>
2024-05-09 20:02:34 -1000

    update

---
+++ ARIMA.py
@@ -0,0 +1,169 @@
+from statsmodels.tsa.stattools import adfuller
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pmdarima.arima.utils import ndiffs
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.stattools import acf
+
+#differencing
+result = adfuller(df.value,dropna())
+plt.rcParams.update({'figure.figsize':(9,7), 'figure.dpi':120})
+
+# Import data
+df = pd.read_csv('', names=['value'], header=0)
+
+# Original Series
+fig, axes = plt.subplots(3, 2, sharex=True)
+axes[0, 0].plot(df.value); axes[0, 0].set_title('Original Series')
+plot_acf(df.value, ax=axes[0, 1])
+
+# 1st Differencing
+axes[1, 0].plot(df.value.diff()); axes[1, 0].set_title('1st Order Differencing')
+plot_acf(df.value.diff().dropna(), ax=axes[1, 1])
+
+# 2nd Differencing
+axes[2, 0].plot(df.value.diff().diff()); axes[2, 0].set_title('2nd Order Differencing')
+plot_acf(df.value.diff().diff().dropna(), ax=axes[2, 1])
+
+plt.show()
+
+
+y = df.value
+
+## Adf Test
+ndiffs(y, test='adf')  # 2
+
+# KPSS test
+ndiffs(y, test='kpss')  # 0
+
+# PP test:
+ndiffs(y, test='pp')  # 2
+
+# PACF plot of 1st differenced series
+plt.rcParams.update({'figure.figsize':(9,3), 'figure.dpi':120})
+
+fig, axes = plt.subplots(1, 2, sharex=True)
+axes[0].plot(df.value.diff()); axes[0].set_title('1st Differencing')
+axes[1].set(ylim=(0,5))
+plot_pacf(df.value.diff().dropna(), ax=axes[1])
+
+plt.show()
+
+
+
+plt.rcParams.update({'figure.figsize':(9,3), 'figure.dpi':120})
+fig, axes = plt.subplots(1, 2, sharex=True)
+axes[0].plot(df.value.diff()); axes[0].set_title('1st Differencing')
+axes[1].set(ylim=(0,1.2))
+plot_acf(df.value.diff().dropna(), ax=axes[1])
+plt.show()
+
+
+# 1,1,2 ARIMA Model
+model = ARIMA(df.value, order=(1,1,2))
+model_fit = model.fit(disp=0)
+print(model_fit.summary())
+
+# 1,1,1 ARIMA Model
+model = ARIMA(df.value, order=(1,1,1))
+model_fit = model.fit(disp=0)
+print(model_fit.summary())
+
+# Plot residual errors
+residuals = pd.DataFrame(model_fit.resid)
+fig, ax = plt.subplots(1,2)
+residuals.plot(title="Residuals", ax=ax[0])
+residuals.plot(kind='kde', title='Density', ax=ax[1])
+plt.show()
+
+# Actual vs Fitted
+model_fit.plot_predict(dynamic=False)
+plt.show()
+
+#find optimal arima 
+# Create Training and Test
+train = df.value[:85]
+test = df.value[85:]
+
+# Build Model
+# model = ARIMA(train, order=(3,2,1))  
+model = ARIMA(train, order=(1, 1, 1))  
+fitted = model.fit(disp=-1)  
+
+# Forecast
+fc, se, conf = fitted.forecast(15, alpha=0.05)  # 95% conf
+
+# Make as pandas series
+fc_series = pd.Series(fc, index=test.index)
+lower_series = pd.Series(conf[:, 0], index=test.index)
+upper_series = pd.Series(conf[:, 1], index=test.index)
+
+# Plot
+plt.figure(figsize=(12,5), dpi=100)
+plt.plot(train, label='training')
+plt.plot(test, label='actual')
+plt.plot(fc_series, label='forecast')
+plt.fill_between(lower_series.index, lower_series, upper_series, 
+                 color='k', alpha=.15)
+plt.title('Forecast vs Actuals')
+plt.legend(loc='upper left', fontsize=8)
+plt.show()
+
+# Build Model
+model = ARIMA(train, order=(3, 2, 1))  
+fitted = model.fit(disp=-1)  
+print(fitted.summary())
+
+# Forecast
+fc, se, conf = fitted.forecast(15, alpha=0.05)  # 95% conf
+
+# Make as pandas series
+fc_series = pd.Series(fc, index=test.index)
+lower_series = pd.Series(conf[:, 0], index=test.index)
+upper_series = pd.Series(conf[:, 1], index=test.index)
+
+# Plot
+plt.figure(figsize=(12,5), dpi=100)
+plt.plot(train, label='training')
+plt.plot(test, label='actual')
+plt.plot(fc_series, label='forecast')
+plt.fill_between(lower_series.index, lower_series, upper_series, 
+                 color='k', alpha=.15)
+plt.title('Forecast vs Actuals')
+plt.legend(loc='upper left', fontsize=8)
+plt.show()
+
+# Accuracy metrics
+def forecast_accuracy(forecast, actual):
+    mape = np.mean(np.abs(forecast - actual)/np.abs(actual))  # MAPE
+    me = np.mean(forecast - actual)             # ME
+    mae = np.mean(np.abs(forecast - actual))    # MAE
+    mpe = np.mean((forecast - actual)/actual)   # MPE
+    rmse = np.mean((forecast - actual)**2)**.5  # RMSE
+    corr = np.corrcoef(forecast, actual)[0,1]   # corr
+    mins = np.amin(np.hstack([forecast[:,None], 
+                              actual[:,None]]), axis=1)
+    maxs = np.amax(np.hstack([forecast[:,None], 
+                              actual[:,None]]), axis=1)
+    minmax = 1 - np.mean(mins/maxs)             # minmax
+    acf1 = acf(fc-test)[1]                      # ACF1
+    return({'mape':mape, 'me':me, 'mae': mae, 
+            'mpe': mpe, 'rmse':rmse, 'acf1':acf1, 
+            'corr':corr, 'minmax':minmax})
+
+forecast_accuracy(fc, test.values)
+
+#> {'mape': 0.02250131357314834,
+#>  'me': 3.230783108990054,
+#>  'mae': 4.548322194530069,
+#>  'mpe': 0.016421001932706705,
+#>  'rmse': 6.373238534601827,
+#>  'acf1': 0.5105506325288692,
+#>  'corr': 0.9674576513924394,
+#>  'minmax': 0.02163154777672227}
+
+#references
+#https://www.machinelearningplus.com/time-series/arima-model-time-series-forecasting-python/
